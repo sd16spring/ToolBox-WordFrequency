48c01adfa5bdd592e876923648c839e6a600cbd7
Benjamin Hill <benjamin.hill@olin.edu>
2016-02-11 14:38:04 -0500

    Added starter code

---
+++ frequency.py
@@ -0,0 +1,24 @@
+""" Analyzes the word frequencies in a book downloaded from
+	Project Gutenberg """
+
+import string
+
+def get_word_list(file_name):
+	""" Reads the specified project Gutenberg book.  Header comments,
+		punctuation, and whitespace are stripped away.  The function
+		returns a list of the words used in the book as a list.
+		All words are converted to lower case.
+	"""
+	pass
+
+def get_top_n_words(word_list, n):
+	""" Takes a list of words as input and returns a list of the n most frequently
+		occurring words ordered from most to least frequently occurring.
+
+		word_list: a list of words (assumed to all be in lower case with no
+					punctuation
+		n: the number of words to return
+		returns: a list of n most frequently occurring words ordered from most
+				 frequently to least frequentlyoccurring
+	"""
+	pass
