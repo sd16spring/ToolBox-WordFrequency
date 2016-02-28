a6d27848d487e94912740b30f67fcede2c7b869a
Erica Lee <erica.lee@students.olin.edu>
2016-02-28 17:13:04 -0500

    Turning in Word Frequency Project Toolbox

--- frequency.py
+++ frequency.py
@@ -1,7 +1,24 @@
 """ Analyzes the word frequencies in a book downloaded from
 	Project Gutenberg """
 
+from pattern.web import *
 import string
+import random
+import operator
+
+
+# url = URL('http://www.gutenberg.org/cache/epub/1342/pg1342.txt')
+# all_gutenberg = url.download()
+
+# start_index = all_gutenberg.index('Chapter 1')
+# end_index = all_gutenberg.index('End of the Project Gutenberg EBook of Pride and Prejudice, by Jane Austen')
+
+# just_book = all_gutenberg[start_index:end_index]
+
+
+# f = open('pride_prejudice.txt', 'w')
+# f.write(just_book)
+# f.close()
 
 def get_word_list(file_name):
 	""" Reads the specified project Gutenberg book.  Header comments,
@@ -9,7 +26,25 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	f = open(file_name, 'r+')
+	
+	lines = ""
+	words = []
+
+	for line in f.read().split("\n"):
+		lines += line.strip(string.punctuation + string.whitespace) + " "
+		for word in line.split():
+			word = word.strip(string.punctuation + string.whitespace)
+			word = word.lower()
+			words.append(word)
+
+			
+
+
+	return words
+
+# print get_word_list('pride_prejudice.txt')
+
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -21,4 +56,15 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+	hist = dict()
+	for word in word_list:
+		hist[word] = hist.get(word, 0) + 1
+
+	res = []
+	sorted_hist = sorted(hist.items(), key=operator.itemgetter(1), reverse = True)
+	for (key,value) in sorted_hist:
+		res.append(key)
+	return res
+
+
+print get_top_n_words(get_word_list('pride_prejudice.txt'), 10)
