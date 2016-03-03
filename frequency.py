016ff9cbf4d7a01461c6205a3883852135c88bf6
TehyaStockman <tehyamarie@gmail.com>
2016-03-02 19:08:45 -0500

    tuning in toolbox-wordfrequency

--- frequency.py
+++ frequency.py
@@ -2,16 +2,58 @@
 	Project Gutenberg """
 
 import string
+from collections import Counter
 
-def get_word_list(file_name):
+def get_word_list(filename):
 	""" Reads the specified project Gutenberg book.  Header comments,
 		punctuation, and whitespace are stripped away.  The function
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	list_words = []
+	fp = processing_file(filename)
 
-def get_top_n_words(word_list, n):
+	for line in fp.split('\n'):
+		process_line(line, list_words)
+
+	return list_words
+
+def process_line(line, list_words):
+	'''gets rid of white space and punctuation and puts words into a dictionary'''
+	line = line.replace('-', ' ')
+	for word in line.split():
+		word = word.strip(string.whitespace + string.punctuation)
+		word = word.lower()
+
+		list_words.append(word)
+
+	return list_words
+
+def processing_file(filename):
+	"""processing initial file, stripping out guttenberg extra stuff"""
+	fp = open(filename)
+	all_text = fp.read()
+	index_start = all_text.find('CONTENTS')
+	index_end = all_text.find('End of Project Gutenberg')
+	content =  all_text[index_start: index_end]
+	
+	new_story = []
+	for line in content.split('\n'):
+		if any_lowercase(line):
+			new_story.append(line)
+			
+	story = ''.join(new_story)
+	return story
+	   
+
+def any_lowercase(s):
+    flag = False
+    for c in s:
+        flag = flag or c.islower()
+    return flag	
+
+
+def get_top_n_words(filename, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
 		occurring words ordered from most to least frequently occurring.
 
@@ -21,4 +63,19 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+
+	hist = Counter(get_word_list(filename))
+	
+	t = []
+	for word, count in hist.iteritems():
+		t.append((count, word))
+		t.sort(reverse = True)
+	
+
+	res = []
+	for count, word in t[0:n]:
+		res.append(word)
+	print res
+   
+
+get_top_n_words('grimm_fairytales.txt', 100)
