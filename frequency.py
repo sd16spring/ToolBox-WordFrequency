bd459a9c9900965a179c68e03384d1b2c60a31e3
lapudvan <lauren.pudvan@gmail.com>
2016-03-06 19:23:17 -0500

    Turning in Word Frequency toolbox

--- frequency.py
+++ frequency.py
@@ -2,6 +2,20 @@
 	Project Gutenberg """
 
 import string
+from pattern.web import *
+import operator
+url = URL('http://www.gutenberg.org/ebooks/161.txt.utf-8')
+all_gutenberg = url.download()
+start_location = all_gutenberg.index('Chapter 1')
+end_location = all_gutenberg.index('End of the Project Gutenberg EBook of Sense and Sensibility, by Jane Austen')
+just_book = all_gutenberg[start_location:end_location]
+
+
+f = open('sense_and_sensibility.txt', 'w')
+f.write(just_book)
+f.close()
+
+
 
 def get_word_list(file_name):
 	""" Reads the specified project Gutenberg book.  Header comments,
@@ -9,7 +23,17 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	f = open(file_name, 'r+')
+	lines = ""
+	words = []
+	for line in f.read().split("\n"):
+		lines += line.strip(string.punctuation + string.whitespace) + " "
+		for word in line.split():
+			word = word.strip(string.punctuation + string.whitespace)
+			word = word.lower()
+			words.append(word)
+	return words
+
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -21,4 +45,17 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+
+    wordcount={} # new dictionary
+    for word in word_list:
+        if word not in wordcount:
+            wordcount[word] = 1
+        else:
+            wordcount[word] += 1
+    sorted_wordcount = sorted(wordcount.items(), key=operator.itemgetter(1), reverse = True)
+    res = [] #list of words in order of most to lesat frequent
+    for (key,value) in sorted_wordcount:
+ 		res.append(key)
+    return res[:n]
+
+print get_top_n_words(get_word_list('sense_and_sensibility.txt'), 100)
