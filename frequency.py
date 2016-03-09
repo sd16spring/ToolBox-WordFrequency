0c7c3268e5f4492970c713ded2fcf59c612cb0bb
Rachel Yang <rachel.yang@students.olin.edu>
2016-03-08 19:54:38 -0500

    Turning in Word Frequency ToolBox

--- frequency.py
+++ frequency.py
@@ -2,14 +2,43 @@
 	Project Gutenberg """
 
 import string
+from nltk.corpus import stopwords
+
 
 def get_word_list(file_name):
+
 	""" Reads the specified project Gutenberg book.  Header comments,
 		punctuation, and whitespace are stripped away.  The function
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	# open book for reading/parsing
+	ft = open(file_name, 'r')
+	
+	# create empty list to append words from book into
+	Grimms_edit = []
+
+	# list of stopwords to filter out boring words
+	stop = set(stopwords.words('english'))
+
+
+	for line in ft:
+		""" for each line in the book, remove punctuation and hyphens """
+		line = line.translate(None, string.punctuation)
+		line = line.replace('-', ' ')
+
+		""" for each word in each line, remove extra whitespace and lowercase all letters """
+		for word in line.split():
+			word = word.strip(string.punctuation + string.whitespace)
+			word = word.lower()
+
+			# filter out boring words
+			if word not in stop:
+				Grimms_edit.append(word)		# append to empty list
+			else:
+				pass
+	get_top_n_words(Grimms_edit, 100)			# top 100 words of filtered words from book
+
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -21,4 +50,24 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+	# initialize dictionary called histogram
+	hist = dict()
+
+	# for each word in the list, find frequency
+	for word in word_list:
+		hist[word] = hist.get(word, 0) + 1
+
+	# create new list of words sorted by frequency
+	t = []
+	for key,value in hist.items():
+		t.append((value, key))
+
+	t.sort(reverse = True)
+
+	# print most common words based on n
+	print 'The most common words are: '
+	for freq,word in t[:n]:
+		print word, '\t', freq
+
+
+get_word_list('GrimmsFairyTales.txt')
