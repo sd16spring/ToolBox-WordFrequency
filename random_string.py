89074eec945766e7f0390f09af62c226fc39f028
mary-keenan <mary.keenan@students.olin.edu>
2016-03-11 17:08:28 -0500

    Locked out of pattern for last day -- random_string.py has a string to show that the word frequency is working; frequency.py should correctly load Pride and Prejudice and find the top 100 words, but I can't do that right now

--- random_string.py
+++ random_string.py
@@ -14,28 +14,22 @@ def get_word_list(file_name):
 	"""
 	#load book, create list of lines
 	text = open(file_name,'r+')
-	text_lines = text.readlines()
-	curr_line = 0
-	while text_lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
-		curr_line += 1
-	text_lines = text_lines[curr_line+1:]
-	#run through lines in list
+	# text_lines = text.readlines()
+	# curr_line = 0
+	# while text_lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
+	# 	curr_line += 1
+	# text_lines = text_lines[curr_line+1:]
+	#separate words in each line, put them in a list
 	word_list = []
-	for line in text_lines:
-		#run through words
-		for word in text.read().split():
-			#go through each character to strip punctuation and whitespace
-			word_now = ""
-			for char in word:
-				if char not in punctuation:
-					if char not in whitespace:
-						#put letters in lowercase
-						char = char.lower()
-						#put word back together
-						word_now += char
-			#make list of words
-			word_list.append(word_now)
-	#return list of words
+	# for line in text_lines:
+	for word in text.read().split():
+		word_now = ""
+		for char in word:
+			if char not in punctuation:
+				if char not in whitespace:
+					char = char.lower()
+					word_now += char
+		word_list.append(word_now)
 	return word_list
 
 def get_top_n_words(word_list, n):
@@ -48,22 +42,19 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	#create dictionary to store key and value (word and frequency)
 	hist = dict()
-	#run through words, put them in a dictionary with their frequency
 	for word in word_list:
 		hist[word] = hist.get(word,0) + 1
 	in_order = []
-	#put them in a list to sort them
 	for word,number in hist.items():
 		in_order.append((number,word))
 	in_order.sort(reverse = True)
-	#return top n words from the list
 	top_n = in_order[0:n]
 	return top_n
 
-PP = URL('http://www.gutenberg.org/cache/epub/1342/pg1342.txt').download()
+PP = 'Add, some code, some that, that calls that the two functions you just wrote so that you get the words'
+#PP = URL('http://www.gutenberg.org/cache/epub/1342/pg1342.txt').download()
 f = open('Pride_and_Prejudice','w')
 f.write(PP)
 f.close()
-print get_top_n_words(get_word_list('Pride_and_Prejudice'),100)
+print get_top_n_words(get_word_list('Pride_and_Prejudice'),4)
