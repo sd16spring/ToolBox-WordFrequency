6bbf9fbfe7755ed8ee69bbf047bc8b86a9a69d96
kuannie1 <anne.ku4@gmail.com>
2016-03-27 15:44:34 -0400

    Updated my work for the WordFrequency ToolBox

--- frequency.py
+++ frequency.py
@@ -3,7 +3,6 @@
 
 import string
 
-
 def get_word_list(file_name):
 	""" Reads the specified project Gutenberg book.  Header comments,
 		punctuation, and whitespace are stripped away.  
@@ -21,33 +20,61 @@ def get_word_list(file_name):
 	lines = lines[current_line+1:]
 
 
-	#improved_lines gives lines without the \n parts
+	#improved_lines gives lines without the \n parts, without the punctuation at the end of each line
 	improved_lines = []
 	for line in lines:
-		improved_lines.append(line.strip())
+		improved_lines.append(line.strip().strip(string.punctuation))
 
 	#returns new list without '' inside
 	improved_lines_without_spaces = []
 	for line in improved_lines:
 		if line != '':
 			improved_lines_without_spaces.append(line.strip(string.punctuation))
-	# print improved_lines_without_spaces
+	
 
-	#Next step: turn all words lowercase
+	#An attempt to strip punctuation that failed:
+	# improved_lines_without_punctuation = []
+	# for line in improved_lines_without_spaces:
+	# 	improved_lines_without_punctuation.append(line.strip('!@#$%^&*():"<>?;'))
+	# print improved_lines_without_punctuation
 
-	#Then: Make list that's a histogram
+	#everything_string is a list with all the words (including repeats)
+	everything_string = ""
+	for a_string in improved_lines_without_spaces:
+		everything_string = everything_string + " " + a_string
+	everything_list = everything_string.strip().lower().split()
+
+	#Making a histogram, format: list of tuples 
+	d = dict() #dictionary of terms:
+	for word in everything_list:
+		if word not in d:
+			d[word] = 1
+		else:
+			d[word] += 1
+	list_of_tuples = d.items()
+
+	#Getting a list of all possible numbers of occurrences given the text
+	frequencies = []
+	for a_word in list_of_tuples:
+		word, occurrence = a_word
+		if occurrence not in frequencies:
+			frequencies.append(occurrence)
+	frequencies.sort(reverse = True)
+		
+	#finding the words with maximum occurrences:
+	sorted_list = []
+	for frequency in frequencies:
+		for word in d:
+			if frequency == d[word]:
+				sorted_list.append(word)
+	return sorted_list
 
 	#Final result: list of words arranged by most used to least used
-
 	fin.close()
 
 
-print get_word_list('prideandprejudice.txt')
-
-
-
-
-
+official_word_list = get_word_list('prideandprejudice.txt')
+#print official_word_list
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -59,6 +86,8 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	complete_word_list = get_word_list(word_list)
 	get_top_n_words = word_list[0:n-1]
-	pass
+	return get_top_n_words
+
+print get_top_n_words(official_word_list, 20)
+
