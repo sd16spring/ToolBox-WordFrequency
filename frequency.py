c3d0178289c7b62a0a3bdd54d36fa532e7779cef
Max Schommer <maximilian.schommer@students.olin.edu>
2016-03-28 12:19:07 -0400

    Here is my toolbox

--- frequency.py
+++ frequency.py
@@ -3,13 +3,33 @@
 
 import string
 
-def get_word_list(file_name):
-	""" Reads the specified project Gutenberg book.  Header comments,
-		punctuation, and whitespace are stripped away.  The function
-		returns a list of the words used in the book as a list.
-		All words are converted to lower case.
+def get_word_list(filename):
 	"""
-	pass
+	Reads the specified project Gutenberg book.  Header comments,
+	punctuation, and whitespace are stripped away.  The function
+	returns a list of the words used in the book as a list.
+	All words are converted to lower case. 
+	"""
+	bookraw = open(filename, 'r')
+	bookstring = ''
+	for line in bookraw:
+		bookstring = bookstring + line
+	bookstring = bookstring.replace("\n", " ")
+	bookstring = bookstring.replace(string.punctuation, "")
+	bookstring = bookstring.lower()
+	booklist = bookstring.split()
+
+	counter = 0
+	while counter < 2:
+		if booklist[0] == '***':
+			counter += 1
+			print counter
+			del booklist[0]
+		else:
+			del booklist[0]
+	return booklist
+
+
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -21,4 +41,14 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+	histogram = {}
+	for word in word_list:
+		histogram[word] = histogram.get(word, 0) + 1
+	hist_list = []
+	ordered_by_frequency = sorted(histogram, key=histogram.get, reverse=True)
+	return ordered_by_frequency[0:n]
+
+def word_frequency_stuff(filename, n)
+	word_list = get_word_list(filename)
+	print get_top_n_words(word_list, n)
+
